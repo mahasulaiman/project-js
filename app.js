957c48e73f502fc949618af57c3de2c16ea65be8
mahasulaiman <mhao_h@hotmail.com>
2021-12-29 16:14:55 +0300

    change in app

--- app.js
+++ app.js
@@ -26,46 +26,49 @@ function shuffle(array) {
 
 
 const cards = document.querySelectorAll(".card")
+let openCard = [];
+
+function match(){
+if(openCard[0].children[0].className == openCard[1].children[0].className){
+    openCard[0].classList.add("match")
+    openCard[1].classList.add("match")
+    openCard = [];
+}
+
+else{
+
+    setTimeout(()=>{
+    openCard[0].classList.remove("open");
+    openCard[1].classList.remove("open");
+    openCard = []; 
+},1000)  
+}
+}
+
+function validClick(card){
+    if(openCard.length < 2 && !card.classList.contains("match") && card.classList.contains("card")){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 console.log(cards);
 
 
 for(let card of cards){
 card.addEventListener("click",(event)=>{
+    if (validClick(event.target)){
     event.target.classList.add("open")
-})
+    openCard.push(event.target)
+    console.log(openCard.length);
+if(openCard.length==2){
+    match();
 }
-
-var flg = true;
-var arr = [];
-
-for(let i=0; i<cards.length; i++){
-    cards.addEventListener('click', function(){
-    if(flg){
-        if(arr.length ==0){
-            card.addEventListener("click",(event)=>{
-                event.target.classList.add("open")
-            })
-        }else if(arr.length ==1){
-            card.addEventListener("click",(event)=>{
-                event.target.classList.add("open")
-            })
-        }
-        if(arr.length == 2){
-        flg = false;
-        }
-    }else{
-        return;
     }
-})
+
 }
-
-
-
-
-
-
-
-
-
-
+)
+}
 
